39308c6e5348d0cdc7853a434776bdb719443c8c
Henry Dexter <henrydexter9012@gmail.com>
2022-02-09 14:13:42 -0500

    add| Mapping expression workspace  layout

--- src/components/MappingWorkspace.js
+++ src/components/MappingWorkspace.js
@@ -1,13 +1,17 @@
 import { Box, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 
+const headerBgColor = "#DCDCDC";
+
 const useStyles = makeStyles((theme) => ({
   rootWrapper: {
     width: "100%",
     },
   headerSection: {
     with: "100%",
-    padding: "0.25rem",
+    padding: "0.25rem 1rem",
+    marginRight: "1rem",
+    backgroundColor: headerBgColor,
   },
   mappingWorkspaceWrapper: {},
 }));
@@ -18,7 +22,7 @@ const MappingWorkspace = () => {
   return (
     <Box className={classes.rootWrapper}>
       <Box className={classes.headerSection}>
-        <Typography variant="h7"> MappingWorkspace </Typography>
+        <Typography variant="h7"> Mapping expression workspace </Typography>
       </Box>
       <Box className={classes.mappingWorkspaceWrapper}></Box>
     </Box>
