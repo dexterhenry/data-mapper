f7ec43a265f9fa61e2d0a038dd06466add209869
Henry Dexter <henrydexter9012@gmail.com>
2022-02-09 12:36:33 -0500

    add| Mapping workspace layout

--- src/components/MappingWorkspace.js
+++ src/components/MappingWorkspace.js
@@ -1,20 +1,28 @@
-import { makeStyles, styled } from "@mui/styles";
+import { Box, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles((theme) => ({
-        rootWrapper: {
-          width: "100%",
-          height: 300,
-          backgroundColor: theme.palette.primary.main,
-        },
-      }));
-      
+  rootWrapper: {
+    width: "100%",
+    },
+  headerSection: {
+    with: "100%",
+    padding: "0.25rem",
+  },
+  mappingWorkspaceWrapper: {},
+}));
 
 const MappingWorkspace = () => {
-        return (
-                <div>
-                        <h3>MappingWorkspace</h3>
-                </div>
-        )
-}
+  const classes = useStyles();
 
-export default MappingWorkspace
+  return (
+    <Box className={classes.rootWrapper}>
+      <Box className={classes.headerSection}>
+        <Typography variant="h7"> MappingWorkspace </Typography>
+      </Box>
+      <Box className={classes.mappingWorkspaceWrapper}></Box>
+    </Box>
+  );
+};
+
+export default MappingWorkspace;
