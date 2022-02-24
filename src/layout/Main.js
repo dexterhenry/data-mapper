4482febbf2c5607bda4d005d4889d0d33b7780d5
Henry Dexter <henrydexter9012@gmail.com>
2022-02-23 21:30:33 -0500

    add| RelationsContextProvider

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ThemeWrapper from "./Theme";
 import MainLayout from "./MainLayout";
+import RelationsContextProvider from "../context/RelationsContext";
 
 const Main = () => {
   return (
     <ThemeWrapper>
-      <MainLayout />
+        <RelationsContextProvider>
+          <MainLayout />
+        </RelationsContextProvider>
     </ThemeWrapper>
   );
 };
