4be887fa36c972143d4c9b48d89936e6136bf213
Henry Dexter <henrydexter9012@gmail.com>
2022-02-23 20:57:34 -0500

    add| relations context

---
+++ src/context/RelationsContext.js
@@ -0,0 +1,66 @@
+import React, { createContext, useState } from "react";
+
+export const RelationsContext = createContext();
+
+const initialCurrentRelation = {
+  sourceItem: null,
+  targetItem: null,
+  startX: 0,
+  startY: 0,
+  endX: 0,
+  endY: 0,
+};
+
+const getPosition = (ref) => {
+  const { left, top } = ref.current.getBoundingClientRect();
+  return { x: left, y: top };
+};
+
+const RelationsContextProvider = ({ children }) => {
+  const [currentRelation, setCurrentRelation] = useState(
+    initialCurrentRelation
+  );
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  const [relations, setRelations] = useState([]);
+
+  const onTargetMouseDown = (e, refSource) => {
+    const { x, y } = getPosition(refSource);
+    setIsDrawing(true)
+     setCurrentRelation({
+        ...currentRelation,
+        sourceItem: refSource?.current.dataset['item'],
+        startX: x,
+        startY: y,
+      });
+  };
+
+  const onSourceMouseUp = (e, refSource) => {
+    const { x, y } = getPosition(refSource);
+
+    // make sure you have a source
+    if (currentRelation.sourceItem) {
+      const addCurrentRelation = {
+        ...currentRelation,
+        targetItem: refSource?.current.dataset['item'],
+        endX: x,
+        endy: y,
+      };
+
+      // add relation
+      setRelations([...relations, addCurrentRelation]);
+    }
+    //Always clean the relation
+    setCurrentRelation(initialCurrentRelation);
+    setIsDrawing(false);
+  };
+
+  const data = { isDrawing, relations, onTargetMouseDown, onSourceMouseUp };
+  return (
+    <RelationsContext.Provider value={data}>
+      {children}
+    </RelationsContext.Provider>
+  );
+};
+
+export default RelationsContextProvider;
