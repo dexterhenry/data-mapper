990b390160ae382e6d8d1e1b898ad69839dce0ae
Henry Dexter <henrydexter9012@gmail.com>
2022-03-17 16:13:01 -0400

    add| line visibility validation in search action

--- src/components/Drawer/DrawLines.js
+++ src/components/Drawer/DrawLines.js
@@ -1,11 +1,33 @@
 import { green } from "@mui/material/colors";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Xarrow from "react-xarrows";
 const lineColor = "#77DD77",
   headColor = green[600];
 
-export default function DrawLines({ relations }) {
-  return relations.map((relation, index) => (
+export default function DrawLines({ relations, sourceData, targetData }) {
+  const [relationsLines, setRelationsLines] = useState([]);
+
+  useEffect(() => {
+    const checkSource = sourceData?.map((source) => source.label);
+    const checkTarget = targetData?.map((target) => target.label);
+    /* Filter the lines so that they are only displayed 
+       when the elements are in the source and the target
+       to prevent error in the view so that the lines
+       are not lost by not having the element visible */
+    const filterRelationsLines = relations.filter((relation) => {
+      const hasSource = checkSource.includes(relation.sourceItem),
+        hasTarget = checkTarget.includes(relation.targetItem);
+
+      return hasSource && hasTarget;
+    });
+
+    /* Only the lines of the relations of the visible elements
+     are shown to avoid errors when filtering the elements 
+     in the search function in each work area */
+    setRelationsLines(filterRelationsLines);
+  }, [sourceData, targetData, relations]);
+
+  return relationsLines.map((relation, index) => (
     <Xarrow
       key={`${relation.sourceItem}-${relation.targetItem}-${index}-${index} `}
       start={relation.sourceItemId} //can be react ref
--- src/components/Drawer/DrawerResult.js
+++ src/components/Drawer/DrawerResult.js
@@ -2,13 +2,20 @@ import React, { useContext } from "react";
 import { RelationsContext } from "../../context/RelationsContext";
 import ResultsTable from "./ResultsTable";
 import DrawLines from "./DrawLines";
+import { FilesContext } from "../../context/FilesContext";
 
 const DrawerResult = () => {
   const { relations } = useContext(RelationsContext);
+  const { sourceData, targetData } = useContext(FilesContext);
+
   return (
     <div>
       {relations.length > 0 && <ResultsTable rows={relations} />}
-      <DrawLines relations={relations} />
+      <DrawLines
+        relations={relations}
+        sourceData={sourceData}
+        targetData={targetData}
+      />
     </div>
   );
 };
