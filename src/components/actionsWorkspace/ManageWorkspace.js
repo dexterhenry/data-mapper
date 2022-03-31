ab570f4008ca5a727b3d4b2ab17162693a414cac
Henry Dexter <henrydexter9012@gmail.com>
2022-03-30 22:38:47 -0400

    add| step context  provider

--- src/components/actionsWorkspace/ManageWorkspace.js
+++ src/components/actionsWorkspace/ManageWorkspace.js
@@ -2,6 +2,7 @@ import IconButton from "@mui/material/IconButton";
 import CopyIcon from "@mui/icons-material/FileCopyOutlined";
 import { useState } from "react";
 import ActionPopupBox from "./ActionPopupBox";
+import StepsContextProvider from "../../context/StepsContext";
 
 export default function ManageWorkspace({ type }) {
   const [open, setOpen] = useState(false);
@@ -13,13 +14,15 @@ export default function ManageWorkspace({ type }) {
       <IconButton aria-label="copy" onClick={() => setOpen(true)}>
         <CopyIcon />
       </IconButton>
-      <ActionPopupBox
-        id="ringtone-menu"
-        type={type}
-        keepMounted
-        open={open}
-        onClose={handleClose}
-      />
+      <StepsContextProvider>
+        <ActionPopupBox
+          id="ringtone-menu"
+          type={type}
+          keepMounted
+          open={open}
+          onClose={handleClose}
+        />
+      </StepsContextProvider>
     </>
   );
 }
