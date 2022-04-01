0e7ccc12a09a5b2ccb33d79b95850b444f52b342
Henry Dexter <henrydexter9012@gmail.com>
2022-03-31 23:06:46 -0400

    add| picklist and interaction on data_type step

--- src/components/actionsWorkspace/Steps/DataTypeStep.js
+++ src/components/actionsWorkspace/Steps/DataTypeStep.js
@@ -4,10 +4,14 @@ import {
   Checkbox,
   FormGroup,
   FormControlLabel,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
 } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import Typography from "@mui/material/Typography";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { StepsContext } from "../../../context/StepsContext";
 import { SOURCE_TYPE, TARGET_TYPE } from "../../Mapping";
 
@@ -25,6 +29,8 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const DataTypeStep = ({ type }) => {
+  const [dataType, setDataType] = useState("");
+
   let form,
     handleChange,
     errors = null;
@@ -49,75 +55,107 @@ const DataTypeStep = ({ type }) => {
     errors = dataTypeStepErrorTarget;
   }
 
+  const jsonType = dataType === "json";
+  const fileType = dataType === "file";
+
+  const handleChangeDataType = (e) => {
+    setDataType(e.target.value);
+    handleChange(e);
+  };
+
+  useEffect(() => {
+    setDataType(form.dataType);
+  }, []);
+
   const classes = useStyles();
 
   return (
     <Grid container spacing={1} className={classes.rootWrapper}>
-      {/* Section for  JSON Types​ */}
       <Grid item xs={12}>
-        <Typography className={classes.titles}>JSON Types​</Typography>
-        <TextField
-          required
-          id={`${type}-data-type-json-types-schema`}
-          label="Schema"
-          value={form?.dataTypeJsonSchema}
-          onChange={handleChange}
-          name="dataTypeJsonSchema"
-          variant="standard"
-          size="small"
-          fullWidth
-        />
-        {errors.dataTypeJsonSchema && (
-          <Typography variant="subtitle1" className={classes.errorsText}>
-            {errors.dataTypeJsonSchema}
-          </Typography>
-        )}
-        <FormGroup>
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={form?.dataTypeJsonDiscardAdditionalParameters}
-                onChange={handleChange}
-                name="dataTypeJsonDiscardAdditionalParameters"
-                size="small"
-              />
-            }
-            label="Discard Additional Parameters"
+        <FormControl fullWidth required variant="standard">
+          <InputLabel id={`${type}-data-type-label`}>
+            Pick a data type
+          </InputLabel>
+          <Select
+            labelId={`${type}-data-type-label`}
+            value={form.dataType}
+            onChange={handleChangeDataType}
+            name="dataType"
+            label="Pick a data type"
+          >
+            <MenuItem value={"json"}>JSON Types​</MenuItem>
+            <MenuItem value={"file"}>File Types</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+      {/* Section for  JSON Types​ */}
+      {jsonType && (
+        <Grid item xs={12} mt={4}>
+          <Typography className={classes.titles}>JSON Types​</Typography>
+          <TextField
+            required
+            id={`${type}-data-type-json-types-schema`}
+            label="Schema"
+            value={form?.dataTypeJsonSchema}
+            onChange={handleChange}
+            name="dataTypeJsonSchema"
+            variant="standard"
             size="small"
+            fullWidth
           />
-        </FormGroup>
-        <TextField
-          id={`${type}-data-type-step-title`}
-          label="Title"
-          value={form?.dataTypeJsonTypeTitle}
-          name="dataTypeJsonTypeTitle"
-          onChange={handleChange}
-          variant="standard"
-          size="small"
-          fullWidth
-        />
-      </Grid>
-
+          {errors.dataTypeJsonSchema && (
+            <Typography variant="subtitle1" className={classes.errorsText}>
+              {errors.dataTypeJsonSchema}
+            </Typography>
+          )}
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={form?.dataTypeJsonDiscardAdditionalParameters}
+                  onChange={handleChange}
+                  name="dataTypeJsonDiscardAdditionalParameters"
+                  size="small"
+                />
+              }
+              label="Discard Additional Parameters"
+              size="small"
+            />
+          </FormGroup>
+          <TextField
+            id={`${type}-data-type-step-title`}
+            label="Title"
+            value={form?.dataTypeJsonTypeTitle}
+            name="dataTypeJsonTypeTitle"
+            onChange={handleChange}
+            variant="standard"
+            size="small"
+            fullWidth
+          />
+        </Grid>
+      )}
       {/* Section for  File Types​ */}
-      <Grid item xs={12} mt={4}>
-        <Typography className={classes.titles}>File Types​</Typography>
-        <TextField
-          required
-          id={`${type}-data-type-step-file-types`}
-          label="Title"
-          value={form?.dataTypeFileTypeTitle}
-          name="dataTypeFileTypeTitle"
-          onChange={handleChange}
-          variant="standard"
-          size="small"
-          fullWidth
-        />
-        {errors.dataTypeFileTypeTitle && (
-          <Typography variant="subtitle1" className={classes.errorsText}>
-            {errors.dataTypeFileTypeTitle}
-          </Typography>
-        )}
-      </Grid>
+      {fileType && (
+        <Grid item xs={12} mt={4}>
+          <Typography className={classes.titles}>File Types​</Typography>
+          <TextField
+            required
+            id={`${type}-data-type-step-file-types`}
+            label="Title"
+            value={form?.dataTypeFileTypeTitle}
+            name="dataTypeFileTypeTitle"
+            onChange={handleChange}
+            variant="standard"
+            size="small"
+            fullWidth
+          />
+          {errors.dataTypeFileTypeTitle && (
+            <Typography variant="subtitle1" className={classes.errorsText}>
+              {errors.dataTypeFileTypeTitle}
+            </Typography>
+          )}
+        </Grid>
+      )}
     </Grid>
   );
 };
